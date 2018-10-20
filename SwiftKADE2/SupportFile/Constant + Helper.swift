946734ed6602ad7da9ef9175c4b79c9fd3e632d1
Ajisaputrars <Ajisaputrars@hotmail.com>
2018-10-20 18:15:06 +0700

    Creating Constant and Helper

---
+++ SwiftKADE2/SupportFile/Constant + Helper.swift
@@ -0,0 +1,11 @@
+//
+//  Constant + Helper.swift
+//  SwiftKADE2
+//
+//  Created by Aji Saputra Raka Siwi on 20/10/18.
+//  Copyright © 2018 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+let accentColor = UIColor(red: 21/255, green: 126/255, blue: 251/255, alpha: 1)
