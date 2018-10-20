5e66317e324a7389b1bfc864750e14f85ddbfc8b
Ajisaputrars <Ajisaputrars@hotmail.com>
2018-10-20 15:53:24 +0700

    Preparing main view from AppDelegate, removing Storyboard

--- SwiftKADE2/AppDelegate.swift
+++ SwiftKADE2/AppDelegate.swift
@@ -17,6 +17,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UIApplication.shared.statusBarStyle = .lightContent
+        let nav1 = UINavigationController()
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0/255, green: 165/255, blue: 255/255, alpha: 1)
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedStringKey.foregroundColor : UIColor.white]
+        let controller = MainController(nibName: "MainController", bundle: nil)
+        nav1.viewControllers = [controller]
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = nav1
+        
         return true
     }
 
---
+++ SwiftKADE2/View/MainView.swift
@@ -0,0 +1,13 @@
+//
+//  MainView.swift
+//  SwiftKADE2
+//
+//  Created by Aji Saputra Raka Siwi on 20/10/18.
+//  Copyright © 2018 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+    
+}
