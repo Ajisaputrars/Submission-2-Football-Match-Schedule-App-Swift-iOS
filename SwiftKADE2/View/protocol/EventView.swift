4e3562d154594e8de2e42ebe6a71f08a77ff6c67
Ajisaputrars <Ajisaputrars@hotmail.com>
2018-10-20 22:47:08 +0700

    Create Event presenter

---
+++ SwiftKADE2/View/protocol/EventView.swift
@@ -0,0 +1,9 @@
+//
+//  EventView.swift
+//  SwiftKADE2
+//
+//  Created by Aji Saputra Raka Siwi on 20/10/18.
+//  Copyright © 2018 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import Foundation
