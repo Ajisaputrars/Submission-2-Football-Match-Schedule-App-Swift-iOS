eb78be1a906785450e1bad9b2b45327c08ec6e4f
Ajisaputrars <Ajisaputrars@hotmail.com>
2018-10-21 00:06:58 +0700

    Preparing Detail page

---
+++ SwiftKADE2/View/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  SwiftKADE2
+//
+//  Created by Aji Saputra Raka Siwi on 21/10/18.
+//  Copyright © 2018 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class DetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
